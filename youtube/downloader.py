bfc03c6ecdf1d94f0258443ab200c4b4d5c989a8
Abhishekkumarpaswan <64888857+Abhishekkumarpaswan@users.noreply.github.com>
2022-08-03 09:27:36 +0530

    Update downloader.py

--- youtube/downloader.py
+++ youtube/downloader.py
@@ -1,9 +1,9 @@
 from os import link
 from pytube import YouTube
-from sys import argv
 
-link = argv[1]
-yt = YouTube[link]
+print("Enter the link to download")
+link = input()
+yt = YouTube(link)
 
 print("Title:",yt.title)
 print("Views:",yt.views)
